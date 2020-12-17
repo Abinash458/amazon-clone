4f62046da981df65559aacb0e89be7cd3db927d0
Abinash Mohapatra <abinash@expensebit.com>
2020-12-17 15:33:36 +0530

    header Design

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8C9XFTWnOJqLfTRmoI6EjDknSr-kgz8s",
+  authDomain: "clone-7cd55.firebaseapp.com",
+  projectId: "clone-7cd55",
+  storageBucket: "clone-7cd55.appspot.com",
+  messagingSenderId: "979958049814",
+  appId: "1:979958049814:web:ae89eecb626757c4b15937",
+  measurementId: "G-6M4PP5RJFF"
+};
